c185d07e38cebd6921af0617d30c1aadc65b1d6a
Thiem <thiem.huynh0@gmail.com>
2023-02-13 21:16:31 -0500

    nodejs installs dev environments and depends

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
+const Pet = (props) => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, props.name),
+        React.createElement("h2", {}, props.animal),
+        React.createElement("h2", {}, props.breed),
+    ]);
+}
+
+
+
+
 const App = () => {
     return React.createElement(
         "div",
         {},
-        React.createElement("h1", {}, "Adopt Me!")
+        [
+            React.createElement("h1", {}, "Adopt Me!"),
+            React.createElement(Pet, {
+                animal: "Dog",
+                name: "Luna",
+                breed: "Havanese"
+            }),
+            React.createElement(Pet, {
+                animal: "Bird",
+                name: "Pepper",
+                breed: "Cockatiel"
+            }),
+            React.createElement(Pet, {
+                animal: "Cat",
+                name: "Doink",
+                breed: "Mixed"
+            }),
+        ]
     )
 };
 
