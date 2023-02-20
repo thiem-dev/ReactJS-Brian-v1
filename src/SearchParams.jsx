1e7ea8217a34de9ace45c5dc97ab01680b9dc1d3
Thiem <thiem.huynh0@gmail.com>
2023-02-20 18:51:10 -0500

    breedlist added to searchParams

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -8,7 +8,7 @@ const SearchParams = () => {
   const [location, setLocation] = useState("");
   const [animal, setAnimal] = useState("");
   const [breed, setBreed] = useState("");
-  const breeds = [];
+  const [breeds] = useBreedList(animal);
 
   useEffect(() => {
     requestPets();
