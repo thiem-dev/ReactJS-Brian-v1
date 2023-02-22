f37e067c4afc307c5a339c45deb7fbc4abaa4020
Thiem <thiem.huynh0@gmail.com>
2023-02-22 18:07:12 -0500

    added fetchBreedList fail protocols

---
+++ src/fetchBreedList.js
@@ -0,0 +1,15 @@
+const fetctBreedList = async ({ queryKey }) => {
+  const animal = queryKey[1];
+
+  if (!animal) return [];
+
+  const apiRes = await fetch(`http://pets-v2.dev-apis.com/animal?id=${animal}`);
+
+  if (!apiRes.ok) {
+    throw new Error(`breeds/${animal} fetch not ok`);
+  }
+
+  return apiRes.json();
+};
+
+export default fetctBreedList;
