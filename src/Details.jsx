d836c63653744e05c20f497c098be87719738e85
Thiem <thiem.huynh0@gmail.com>
2023-02-22 17:56:47 -0500

    querykey fetch and typo fixed

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,8 +1,37 @@
 import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import fetchPet from "./fetchPet";
 
 const Details = () => {
   const { id } = useParams();
-  return <h2>{id}</h2>;
+  const results = useQuery(["details", id], fetchPet);
+
+  if (results.isError) {
+    return <h2>oh no! Results errored</h2>;
+  }
+
+  if (results.isLoading) {
+    return (
+      <div className="loading-pane">
+        <h2 className="loader">🌀</h2>
+      </div>
+    );
+  }
+
+  const pet = results.data.pets[0];
+
+  return (
+    <div className="details">
+      <div>
+        <h1>{pet.name}</h1>
+        <h2>
+          {pet.name} - {pet.breed} - ${pet.city}, ${pet.state}
+          <button>Adopt {pet.name}</button>
+          <p>{pet.description}</p>
+        </h2>
+      </div>
+    </div>
+  );
 };
 
 export default Details;
