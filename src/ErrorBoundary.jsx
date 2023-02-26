ccb9fd67908a2e741fd879b7bb49e90f4fafba8e
Thiem <thiem.huynh0@gmail.com>
2023-02-26 10:36:00 -0500

    fixed error boundarytypo

--- src/ErrorBoundary.jsx
+++ src/ErrorBoundary.jsx
@@ -2,13 +2,13 @@ import { Component } from "react";
 import { Link } from "react-router-dom";
 
 class ErrorBoundary extends Component {
-  state = { hasErrr: false };
+  state = { hasError: false };
   static getDerivedStateFromError() {
     return { hasError: true };
   }
 
   componentDidCatch(error, info) {
-    // typically you would log this to something like TrackJS ro NewRelic
+    // typically you would log this to something like TrackJS or NewRelic
     console.error("ErrorBoundary component caught an error", error, info);
   }
 
